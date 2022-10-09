b08fdf577430163b957b61cced482de312c07edf
wujia777 <cwu005104@gmail.com>
2022-10-09 23:57:33 +0800

    feat: add .ignore & handlebars

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
-app.get('/language/:language', (req, res) => {
-    console.log(req.params.language)
-    res.send(`<h1>params language is: ${req.params.language}</h1>`)
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
 app.listen(port, () => {
